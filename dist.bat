a28585079c7318cf73f99a191f1b0663ed9dd65f
Mark Green <mark@antelope.nildram.co.uk>
2016-06-22 22:09:33 +0100

    Dist builder.

---
+++ dist.bat
@@ -0,0 +1,9 @@
+call make.bat
+copy StrikeGen.html dist
+copy Strikegen.js dist
+copy download.js dist
+copy style.css dist
+mkdir dist\icons
+copy icons dist\icons
+mkdir dist\data
+copy data dist\data
