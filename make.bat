0a02ccdce08913ae522704838fce4cf1a2aa62d9
Mark Green <mark@antelope.nildram.co.uk>
2016-07-12 01:14:49 +0100

    Use closure-compiler to shrink js

--- make.bat
+++ make.bat
@@ -1,1 +1,3 @@
-elm make Main.elm --output=StrikeGen.js
+call elm make Main.elm --output=StrikeGen.js
+rename StrikeGen.js StrikeGenUncompiled.js
+java -jar \Java\closure-compiler\compiler.jar -O ADVANCED --js StrikeGenUncompiled.js --js_output_file StrikeGen.js
