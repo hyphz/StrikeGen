232312888435fa48a361a14beea993435e8fb2bb
Mark Green <mark@antelope.nildram.co.uk>
2016-10-24 15:58:02 +0100

    Google Analytics for online version.

--- fulldist.bat
+++ fulldist.bat
@@ -3,6 +3,7 @@ cd dist
 "\program files\winzip\wzzip" -a -r -o -P -ex ..\web\StrikeGen.zip *.*
 cd..
 xcopy dist web /s /y
+copy StrikeGenWithTracking.html web\StrikeGen.html
 copy SiteIndex.html web\index.html
 cd web
 git add .
