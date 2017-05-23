80b3aeeaee58e76353b5a604a8a83f23cd211d92
hyphz <drmoose94@gmail.com>
2017-05-23 19:55:35 +0100

    Update elm core library version and closure compiler compatibility.

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 call elm make Main.elm --output=StrikeGenUncompiled.js
-java -jar \Java\closure-compiler\compiler.jar -O ADVANCED --js StrikeGenUncompiled.js --js_output_file StrikeGen.js
+java -jar \Java\closure-compiler\compiler.jar -O ADVANCED --jscomp_off=checkVars --js StrikeGenUncompiled.js --js_output_file StrikeGen.js
